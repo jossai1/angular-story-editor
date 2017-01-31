6968735f5e831ad9053f421df16be1a3f41a465c
Jane Ossai <buff_jane@hotmail.com>
2017-01-31 20:45:15 +0000

    add in provjs library can make a basic prov doc (within code and display in console)

--- src/app/test-model/test-model.component.ts
+++ src/app/test-model/test-model.component.ts
@@ -9,18 +9,35 @@ export class TestModelComponent implements OnInit {
 
   constructor() { }
 
+  prov:any = require('../../../provjs/prov');
+  doc:any =  this.prov.document();
+  amber:number = 0;
+  ex = this.doc.addNamespace("ex", "http://www.example.org#");
+  dcterms = this.doc.addNamespace("dcterms", "http://purl.org/dc/terms/");
+  foaf = this.doc.addNamespace("foaf", "http://xmlns.com/foaf/0.1/");
+  
   ngOnInit() {
+  	console.log(this.doc);
+  }
+
+  setNameSpace(){
+  	// Prefix declarations
   }
 
   addActor(){
-
+  	this.doc.agent("ex:derek",
+      ["prov:type", this.prov.ns.Person, "foaf:givenName", "Derek",
+       "foaf:mbox", "<mailto:derek@example.org>"]);
   }
 
   addEvent(){
+  	// Activities
+    this.doc.activity("ex:compile1");
   	
   }
 
-  addThing(){
+  addThing() { 
+  	this.doc.entity("ex:article", ["dcterms:title", "Crime rises in cities"]);
   	
   }
 
@@ -29,8 +46,15 @@ export class TestModelComponent implements OnInit {
   	
   }
 
+  getDoc(){
+  	return this.doc.scope;
+  }
+
+  getProcJSON(){
+  	return this.getDoc().getProvJSON();
+  }
   export(){
-  	
+  	console.log(this.getDoc());
   }
 
 
