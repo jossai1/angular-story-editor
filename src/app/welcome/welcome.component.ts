c6df42a734b10e336020fe2502cf6f971efcc0d7
Jane Ossai <buff_jane@hotmail.com>
2017-03-15 00:56:53 +0000

    tutorial for welcome page

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -9,7 +9,34 @@ export class WelcomeComponent implements OnInit {
 
   constructor() { }
 
+  Shepherd:any = require('../../../node_modules/tether-shepherd/dist/js/shepherd');
+  tour:any;
   ngOnInit() {
+ 
+	this.tour = new this.Shepherd.Tour({
+	  defaults: {
+	    classes: 'shepherd-theme-arrows',
+	    scrollTo: true
+	  }
+	});
+
+	this.tour.addStep('example-step', {
+	  text: 'Click to go to the Story Editor',
+	  attachTo: '.create-story bottom',
+	  buttons: [
+	    
+	  ]
+	});
+
+	this.tour.start();
+
+
+  }
+
+
+  cancelTour()
+  {
+  	this.tour.cancel();
   }
 
 }
