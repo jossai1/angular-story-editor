30b9bd1222a07b5905c53944d3b12f60a2d684fb
Jane Ossai <buff_jane@hotmail.com>
2017-04-27 01:19:37 +0100

    removed comments from embedly api service leaving in console.log code so i know if the srevice is being contacted

--- src/app/Services/embedly-service.service.ts
+++ src/app/Services/embedly-service.service.ts
@@ -4,7 +4,6 @@ import 'rxjs/add/operator/toPromise';
 	
 
 
-
 @Injectable()
 export class EmbedlyService {
 
@@ -13,20 +12,11 @@ export class EmbedlyService {
 	EMBEDLY_KEY = '9d73e061a5b14fb493300383ac612299';
 	embedlyApi:string = "https://api.embedly.com/1/extract?key=:9d73e061a5b14fb493300383ac612299&url="
 
-
-	//
-	checkUrl() {
-
-	}
-
 	/***
 	* takes a url and returns a obj containing a summary data of that URL 
 	*/
 	getUrlSummary(url:string) :Promise<any> {
 
-		//check that url is in correct format
-		// call single url
-		//var url = 'http://www.youtube.com/watch?v=Zk7dDekYej0';
 		let QueryUrl =  this.embedlyApi + url;
 	    console.log("service:",'getting url summary');
 	    return this.http.get(QueryUrl)
@@ -34,13 +24,12 @@ export class EmbedlyService {
 	                 .then(response => response.json())
 	                 .catch(this.handleError);
 
-  }
+  	}
 
 
-  private handleError(error: any) {
-     console.error('An error occurred', error);
-     return Promise.reject(error.message || error);
-  }
-
+	private handleError(error: any) {
+		console.error('An error occurred', error);
+		return Promise.reject(error.message || error);
+	}
 
 }
